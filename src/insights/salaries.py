eabf54710c2fc3e3e8383018266d29353ad1b008
nataliakoliveira <nataliaakellyy@gmail.com>
2023-05-01 11:57:18 -0300

    test

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -3,39 +3,36 @@ from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-        salary = read(path)
-        unique_salary = set()
-        for job in salary:
-            salary_type = job.get("max_salary")
-            if salary_type != "" and salary_type.isdigit():
-                unique_salary.add(int(salary_type))
-        return max(unique_salary)
+    salary = read(path)
+    unique_salary = set()
+    for max_val in salary:
+        salary_type = max_val.get("max_salary")
+        if salary_type != "" and salary_type.isdigit():
+           unique_salary.add(int(salary_type))
+    return max(unique_salary)
 
 
 def get_min_salary(path: str) -> int:
-        salary = read(path)
-        unique_salary = set()
-        for job in salary:
-            salary_type = job.get("min_salary")
-            if salary_type != "" and salary_type.isdigit():
-                unique_salary.add(int(salary_type))
-        return min(unique_salary)
+    salary = read(path)
+    unique_salary = set()
+    for job in salary:
+        salary_type = job.get("min_salary")
+        if salary_type != "" and salary_type.isdigit():
+            unique_salary.add(int(salary_type))
+    return min(unique_salary)
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
     """Checks if a given salary is in the salary range of a given job
-
     Parameters
     ----------
     job : dict
         The job with `min_salary` and `max_salary` keys
     salary : int
         The salary to check if matches with salary range of the job
-
     Returns
     -------
     bool
         True if the salary is in the salary range of the job, False otherwise
-
     Raises
     ------
     ValueError
@@ -52,14 +49,12 @@ def filter_by_salary_range(
     salary: Union[str, int]
 ) -> List[Dict]:
     """Filters a list of jobs by salary range
-
     Parameters
     ----------
     jobs : list
         The jobs to be filtered
     salary : int
         The salary to be used as filter
-
     Returns
     -------
     list
